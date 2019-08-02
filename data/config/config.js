428bcca695836aac186796d74985b2430d57fbf6
Daniel Uche <dank.uche@yahoo.com>
2019-08-02 15:43:32 +0100

    updated sequelize config to use query string

--- data/config/config.js
+++ data/config/config.js
@@ -1,27 +1,19 @@
 const fs = require('fs');
+require('dotenv').config();
 
 const DATABASE_URL = process.env.DATABASE_URL;
 
 module.exports = {
   development: {
-    DATABASE_URL,
-    // username: '',
-    // password: 'database_dev',
-    // database: 'database_dev',
-    // host: '127.0.0.1',
-    dialect: 'mysql',
+    url: DATABASE_URL,
+    dialect: 'postgres',
   },
   test: {
-    DATABASE_URL,
-    dialect: 'mysql',
+    url: DATABASE_URL,
+    dialect: 'postgres',
   },
   production: {
-    url: '',
-    DATABASE_URL,
-    // username: process.env.DB_USERNAME,
-    // password: process.env.DB_PASSWORD,
-    // database: process.env.DB_NAME,
-    // host: process.env.DB_HOSTNAME,
-    dialect: 'mysql',
+    url: DATABASE_URL,
+    dialect: 'postgres',
   },
 };
